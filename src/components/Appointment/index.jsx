024db0464ac2d5683570d66d48e79c0fdf5dedb1
Logan Woolf <loganwoolf@gmail.com>
2022-02-24 14:29:39 -0700

    Display appointments in component

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 import 'components/Appointment/style.scss'
 
+import Header from './Header'
+import Show from './Show'
+import Empty from './Empty'
+
 export default function Appointment(props) {
-  const { time } = props
-  return <article className="appointment">Appointment at {time}</article>
+  const { interview, time } = props
+  
+  return (
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? (
+        <Show
+          {...interview}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  )
 }
