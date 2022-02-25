b0f2fcadaa26a6b910a8afa909fc527c2347fb1c
Logan Woolf <loganwoolf@gmail.com>
2022-02-24 20:33:32 -0700

    Get week data from API

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Appointment from './Appointment'
 import DayList from './DayList'
@@ -6,23 +7,23 @@ import DayList from './DayList'
 import 'components/Application.scss'
 
 // mock data
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-]
+// const days = [
+//   {
+//     id: 1,
+//     name: 'Monday',
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: 'Tuesday',
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: 'Wednesday',
+//     spots: 0,
+//   },
+// ]
 
 const appointments = [
   {
@@ -65,6 +66,13 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
+  const [days, setDays] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+      .then(res => console.log(res.data) || setDays(res.data))
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
