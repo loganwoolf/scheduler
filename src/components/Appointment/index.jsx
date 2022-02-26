b8c069e3764d33e2629ddd51463c28bc6157e006
Logan Woolf <loganwoolf@gmail.com>
2022-02-25 21:14:40 -0700

    Conditionally render Empty and Show

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import useVisualMode from 'hooks/useVisualMode'
+
 import 'components/Appointment/style.scss'
 
 import Header from './Header'
@@ -7,17 +9,25 @@ import Show from './Show'
 import Empty from './Empty'
 
 export default function Appointment(props) {
+  const EMPTY = 'EMPTY'
+  const SHOW = 'SHOW'
+
   const { interview, time } = props
-  
+  const initial = interview ? SHOW : EMPTY
+  const { mode, transition, back } = useVisualMode(initial)
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? (
+      {/* {interview ? <Show {...interview} /> : <Empty />} */}
+      {mode === EMPTY && <Empty onAdd={() => console.log('clicked onAdd')} />}
+      {mode === SHOW && (
         <Show
-          {...interview}
+          student={interview.student}
+          interviewer={interview.interviewer}
+          onEdit={() => console.log('clicked onEdit')}
+          onDelete={() => console.log('clicked onDelete')}
         />
-      ) : (
-        <Empty />
       )}
     </article>
   )
