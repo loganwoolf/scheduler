d1c9723616a3ae07c9f4bb8a28c863dda66c2d2c
Logan Woolf <loganwoolf@gmail.com>
2022-02-25 17:34:33 -0700

    Add getInterview selector for Appointment props

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -5,3 +5,12 @@ export function getAppointmentsForDay(state, day) {
     ? selectedDay.appointments.map(apptId => state.appointments[apptId])
     : []
 }
+
+export function getInterview(state, interview) {
+  return interview && interview.interviewer
+    ? {
+        ...interview,
+        interviewer: state.interviewers[interview.interviewer],
+      }
+    : null
+}
