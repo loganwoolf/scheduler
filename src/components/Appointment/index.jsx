9dac4e711c2d61ad17646068f2be08490496c235
Logan Woolf <loganwoolf@gmail.com>
2022-02-25 21:33:49 -0700

    Added transition to Form/create

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -7,10 +7,12 @@ import 'components/Appointment/style.scss'
 import Header from './Header'
 import Show from './Show'
 import Empty from './Empty'
+import Form from './Form'
 
 export default function Appointment(props) {
   const EMPTY = 'EMPTY'
   const SHOW = 'SHOW'
+  const CREATE = 'CREATE'
 
   const { interview, time } = props
   const initial = interview ? SHOW : EMPTY
@@ -20,13 +22,24 @@ export default function Appointment(props) {
     <article className="appointment">
       <Header time={time} />
       {/* {interview ? <Show {...interview} /> : <Empty />} */}
-      {mode === EMPTY && <Empty onAdd={() => console.log('clicked onAdd')} />}
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => transition(CREATE)}
+        />
+      )}
       {mode === SHOW && (
         <Show
           student={interview.student}
           interviewer={interview.interviewer}
           onEdit={() => console.log('clicked onEdit')}
           onDelete={() => console.log('clicked onDelete')}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => console.log('clicked onSave')}
+          onCancel={() => back()}
         />
       )}
     </article>
