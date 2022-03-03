a14a53aa6a0d30c0652e30f111c4ec5608cd392e
Logan Woolf <loganwoolf@gmail.com>
2022-03-02 23:06:55 -0700

    Prettier on src/helpers

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,24 +1,33 @@
 export function getAppointmentsForDay(state, day) {
-  const selectedDay = state.days.filter(x => x.name === day)[0]
+  const selectedDay = state.days.filter(
+    x => x.name === day
+  )[0];
 
   return selectedDay
-    ? selectedDay.appointments.map(apptId => state.appointments[apptId])
-    : []
+    ? selectedDay.appointments.map(
+        apptId => state.appointments[apptId]
+      )
+    : [];
 }
 
 export function getInterviewersForDay(state, day) {
-  const selectedDay = state.days.filter(x => x.name === day)[0]
+  const selectedDay = state.days.filter(
+    x => x.name === day
+  )[0];
 
   return selectedDay
-    ? selectedDay.interviewers.map(interviewer => state.interviewers[interviewer])
-    : []
+    ? selectedDay.interviewers.map(
+        interviewer => state.interviewers[interviewer]
+      )
+    : [];
 }
 
 export function getInterview(state, interview) {
   return interview && interview.interviewer
     ? {
         ...interview,
-        interviewer: state.interviewers[interview.interviewer],
+        interviewer:
+          state.interviewers[interview.interviewer],
       }
-    : null
+    : null;
 }
